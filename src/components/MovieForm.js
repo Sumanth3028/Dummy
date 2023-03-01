08e7dba83a097e0bc144b91373550c09a1a1d3e8
Sumanth3028 <sumanthakula05@gmail.com>
2023-03-01 18:45:05 +0530

    moving to backend

--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -1,16 +1,25 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useRef } from "react";
 import "./MovieForm.css";
 
-const MovieForm = () => {
+const MovieForm = (props) => {
+  const titleRef = useRef("");
+  const openingRef = useRef("");
+  const releaseRef = useRef("");
+
   const submitHandler = (event) => {
     event.preventDefault();
-    const text = document.getElementById("text").value;
-    const opening = document.getElementById("description").value;
-    const release = document.getElementById("release").value;
-    let obj = { text: text, opening: opening, release: release };
 
-    console.log(obj);
+    const movie = {
+      id:Math.random().toString(),
+      title: titleRef.current.value,
+      openingText: openingRef.current.value,
+      releaseDate: releaseRef.current.value,
+    };
+
+    props.onAddMovie(movie)
   };
+
+  
 
   return (
     <Fragment>
@@ -19,17 +28,18 @@ const MovieForm = () => {
           <label htmlFor="text" className="form label">
             <b>Title</b>
           </label>
-          <input type="text" id="text" className="title" />
+          <input type="text" id="text" className="title" ref={titleRef} />
           <label htmlFor="description" className="form label">
             <b>Opening Text</b>
           </label>
-          <input type="text" id="description" className=" description" />
+          <input type="text" id="description" className=" description" ref={openingRef} />
           <label htmlFor="release" className="form label">
             <b>Release Date</b>
           </label>
-          <input type="text" id="release" className="release" />
+          <input type="text" id="release" className="release" ref={releaseRef} />
           <button type="submit">Add Movie</button>
         </form>
+        
       </section>
     </Fragment>
   );
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -4,14 +4,17 @@ import Movie from './Movie';
 import classes from './MovieList.module.css';
 
 const MovieList = (props) => {
+  
   return (
     <ul className={classes['movies-list']}>
       {props.movies.map((movie) => (
         <Movie
           key={movie.id}
+          id={movie.id}
           title={movie.title}
           releaseDate={movie.releaseDate}
           openingText={movie.openingText}
+          onRemove={props.onDelete}
         />
       ))}
     </ul>
